2f0775e53ee125c02ba065092df8dd292c1c850a
jesuo <jesuortega92@gmail.com>
2025-11-30 13:58:17 -0500

    Mejorando los detalles del modal.

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -3,7 +3,7 @@ const mssql = require('mssql');
 const config ={
     user: 'pruebasNode',
     password: 'Securitas+1234',
-    server:'BOGIT0502P',
+    server:'DESKTOP-93DS6HF',
     database: 'facturas',
     options: {
     encrypt: false, // usa true si tienes SSL
